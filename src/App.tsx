9b0ed5b613c57b256817bcee4ee07a2a268484e0
Hassan <muhammadhassanchannel786@gmail.com>
2023-06-15 14:13:14 +0500

    basic project setup & libraries installed

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar";
+import QuoteApp from "./components/Test";
+
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <QuoteApp />
+    </div>
+  );
+}
+
+export default App;
