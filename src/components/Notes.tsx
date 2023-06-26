0f09e741390311903b05bf8ed7e0693074bc7a77
Hassan <muhammadhassanchannel786@gmail.com>
2023-06-26 16:13:13 +0500

    optimised code

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -1,38 +1,30 @@
-import { useState } from 'react'
+import { useState } from "react";
 import { Draggable } from "react-beautiful-dnd";
+import toast from "react-hot-toast";
+
+import { db } from "../config/firebaseConfig.ts";
+import { collection, doc, updateDoc, deleteDoc } from "firebase/firestore";
+
 import { INote, INotesList } from "../interfaces/NotesInterface";
 
-import toast from "react-hot-toast";
 import { MdDeleteOutline } from "react-icons/md";
 import { BiPencil } from "react-icons/bi";
 
-import { db } from "../config/firebaseConfig.ts";
-import {
-  collection,
-  doc,
-  updateDoc,
-  deleteDoc,
-} from "firebase/firestore";
 
 type NoteProps = {
-    userId: string,
-    container: INotesList,
-    containerIndex: number,
-    note: INote,
-    index: number,
-    state: INotesList[],
-    setState: (newState: INotesList[]) => void;
-}
+  userId: string;
+  container: INotesList;
+  containerIndex: number;
+  note: INote;
+  index: number;
+  state: INotesList[];
+  setState: (newState: INotesList[]) => void;
+};
 
 const Note = (props: NoteProps) => {
   const [hover, setHover] = useState<string>("");
-  console.log(props.state)
 
-  const handleContentChangeNote = (
-    e: React.ChangeEvent<HTMLInputElement>,
-    objectId: string,
-    noteId: string
-  ) => {
+  const handleContentChangeNote = (e: React.ChangeEvent<HTMLInputElement>, objectId: string, noteId: string) => {
     const updatedData = props.state.map((object) => {
       if (object.id === objectId) {
         const updatedNotes = object.notes.map((note) => {
@@ -50,21 +42,11 @@ const Note = (props: NoteProps) => {
 
   const editContent = async (changeContainer: INotesList, noteId: string) => {
     if (props.userId) {
-      const noteRef = doc(
-        db,
-        "users",
-        props.userId,
-        "noteContainer",
-        changeContainer.id,
-        "notes",
-        noteId
-      );;
+      const noteRef = doc(db, "users", props.userId, "noteContainer", changeContainer.id, "notes", noteId);
       if (changeContainer) {
         const note = changeContainer.notes.find((nt) => nt.id === noteId);
         if (note) {
-          const newContent = {
-            content: note.content,
-          };
+          const newContent = { content: note.content };
           await updateDoc(noteRef, newContent);
         }
       }
@@ -72,7 +54,6 @@ const Note = (props: NoteProps) => {
   };
 
   const toggleEditModeNote = (changeContainer: INotesList, noteId: string) => {
-    console.log(props.state)
     const updatedData = props.state.map((object) => {
       if (object.id === changeContainer.id) {
         const updatedNotes = object.notes.map((note) => {
@@ -90,21 +71,13 @@ const Note = (props: NoteProps) => {
     editContent(changeContainer, noteId);
   };
 
-
   const deleteElement = async (contanerId: string, noteId: string) => {
     if (props.userId) {
       try {
-        console.log('deleting')  
+        console.log("deleting");
         const userDocRef = doc(db, "users", props.userId);
-        const noteContainerRef = doc(
-          userDocRef,
-          "noteContainer",
-          contanerId
-        );
-        const noteRef = doc(
-          collection(noteContainerRef, "notes"),
-          noteId
-        );
+        const noteContainerRef = doc(userDocRef, "noteContainer", contanerId);
+        const noteRef = doc(collection(noteContainerRef, "notes"), noteId);
 
         await deleteDoc(noteRef);
       } catch (error) {
@@ -125,59 +98,27 @@ const Note = (props: NoteProps) => {
   return (
     <Draggable key={props.note.id} draggableId={props.note.id} index={props.index}>
       {(provided, snapshot) => (
-        <div
-          className={`note ${
-            snapshot.isDragging ? "bg-yellow-300 skew-y-6" : "bg-white skew-y-0"
-          } select-none rounded-xl my-2 flex item-center px-3 py-2 overflow-auto`}
-          onMouseEnter={() => setHover(`${props.note.id}`)}
-          onMouseLeave={() => setHover("")}
-          ref={provided.innerRef}
-          {...provided.draggableProps}
-          {...provided.dragHandleProps}
-          style={{ ...provided.draggableProps.style }}
-        >
+        <div className={`note ${snapshot.isDragging ? "bg-yellow-300 skew-y-6" : "bg-white skew-y-0"} select-none rounded-xl my-2 flex item-center px-3 py-2 overflow-auto`}
+          onMouseEnter={() => setHover(`${props.note.id}`)} onMouseLeave={() => setHover("")}
+          ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} style={{ ...provided.draggableProps.style }}>
+
           <div className="w-full flex justify-between">
-            {props.note.content.substring(0, 38) ===
-            "https://firebasestorage.googleapis.com" ? (
-              <img src={props.note.content} className="h-20" />
-            ) : (
+            {props.note.content.substring(0, 38) === "https://firebasestorage.googleapis.com" ? <img src={props.note.content} className="h-20" /> : (
+              
               <div>
-                {props.note.isEditable ? (
-                  <input
-                    type="text"
-                    value={props.note.content}
-                    onChange={(e) => handleContentChangeNote(e, props.container.id, props.note.id)}
-                    onBlur={() => toggleEditModeNote(props.container, props.note.id)}
-                    className="bg-[white] pl-[0.58rem] py-1 border-none"
-                    autoFocus
-                  />
-                ) : (
-                  <span className="pl-3">{props.note.content}</span>
-                )}
+                {props.note.isEditable ?
+                  <input type="text" value={props.note.content} onChange={(e) => handleContentChangeNote(e, props.container.id, props.note.id)} onBlur={() =>toggleEditModeNote(props.container, props.note.id)} className="bg-[white] pl-[0.58rem] py-1 border-none" autoFocus />
+                : <span className="pl-3">{props.note.content}</span>
+                }
               </div>
+
             )}
-            <div
-              className={`buttons flex gap-2 ${
-                hover.includes(props.note.id) ? "block" : "hidden"
-              } ${props.note.isEditable ? "hidden" : "block"}`}
-            >
-              <button
-                type="button"
-                onClick={() => toggleEditModeNote(props.container, props.note.id)}
-              >
-                <BiPencil />
-              </button>
-              <button
-                type="button"
-                onClick={() => {
-                //   deleteElement(props.containerIndex, props.index);
-                  deleteElement(props.container.id, props.note.id);
-                }}
-              >
-                <MdDeleteOutline />
-              </button>
+            <div className={`buttons flex gap-2 ${hover.includes(props.note.id) ? "block" : "hidden"} ${props.note.isEditable ? "hidden" : "block"}`}>
+              <button type="button" onClick={() => toggleEditModeNote(props.container, props.note.id)}><BiPencil /></button>
+              <button type="button" onClick={() => { deleteElement(props.container.id, props.note.id)}}><MdDeleteOutline /></button>
             </div>
           </div>
+
         </div>
       )}
     </Draggable>
--- src/components/NotesDisplay.tsx
+++ src/components/NotesDisplay.tsx
@@ -9,7 +9,7 @@ import { ItemType } from "../types/NoteType";
 import Gradient from "./Gradient";
 import { AuthContext } from "../context/authContext.tsx";
 
-import { db, storage } from "../config/firebaseConfig.ts";
+import { db } from "../config/firebaseConfig.ts";
 import {
   collection,
   doc,
@@ -18,14 +18,12 @@ import {
   addDoc,
   onSnapshot,
 } from "firebase/firestore";
-import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
 import Loader from "./Loader.tsx";
-import NoteCard from "./NoteCard.tsx";
+import NoteCard from "./NoteCards.tsx";
 
 function NotesDisplay() {
   const [fetching, setFetching] = useState(false);
-  // const [hover, setHover] = useState<string>("");
   const [state, setState] = useState<INotesList[]>([]);
 
   const { user } = useContext(AuthContext);
@@ -50,10 +48,8 @@ function NotesDisplay() {
 
           try {
             const unsub2 = onSnapshot(collection(noteContainerDoc.ref, "notes"), (notesQuerySnapshot) => {
-              // console.log(notesQuerySnapshot.docs[notesQuerySnapshot.docs.length-1].id)
               console.log(noteContainer)
               noteContainer.notes = [];
-              // console.log(noteContainerDoc)
               notesQuerySnapshot.docs.map((noteDoc) => {
                 const noteData = noteDoc.data();
                 console.log(noteData)
@@ -76,10 +72,8 @@ function NotesDisplay() {
           noteContainers.push(noteContainer);
         });
 
-        // Sort noteContainers based on the index
         noteContainers.sort((a, b) => a.index - b.index);
 
-        // Sort notes array inside each container based on the index
         noteContainers.forEach((container) => {
           container.notes.sort((a, b) => a.index - b.index);
         });
@@ -97,96 +91,9 @@ function NotesDisplay() {
       }
     });
   };
+
   useEffect(() => {
-    
 
-    // const getUserNoteContainers = (userId: string) => {
-    //   // setFetching(true);
-    //   const userDocRef = doc(db, "users", userId);
-    //   const noteContainerRef = collection(userDocRef, "noteContainer");
-    //   onSnapshot(noteContainerRef, (noteContainerQuerySnapshot) => {
-    //     try {
-    //       const noteContainers: INotesList[] = [];
-
-    //       noteContainerQuerySnapshot.docs.map((noteContainerDoc) => {
-    //         const noteContainerData = noteContainerDoc.data();
-    //         const noteContainer: INotesList = {
-    //           id: noteContainerDoc.id.toString(),
-    //           name: noteContainerData.name,
-    //           notes: [],
-    //           isEditable: noteContainerData.isEditable,
-    //           index: noteContainerData.index,
-    //         };
-
-    //         const notesRef = collection(noteContainerDoc.ref, "notes");
-    //         onSnapshot(notesRef, (notesQuerySnapshot) => {
-    //           notesQuerySnapshot.docs.forEach((noteDoc) => {
-    //             const noteData = noteDoc.data();
-    //             const note: INote = {
-    //               id: noteDoc.id,
-    //               content: noteData.content,
-    //               isEditable: noteData.isEditable,
-    //               index: noteData.index,
-    //             };
-
-    //             noteContainer.notes.push(note);
-    //           });
-    //         });
-    //         // const notesUnsubscribe = onSnapshot(notesRef, (notesSnapshot) => {
-    //         //   noteContainer.notes = [];
-      
-    //         //   notesSnapshot.forEach((noteDoc) => {
-    //         //     const noteData = noteDoc.data();
-    //         //     const note: INote = {
-    //         //       id: noteDoc.id,
-    //         //       content: noteData.content,
-    //         //       isEditable: noteData.isEditable,
-    //         //       index: noteData.index,
-    //         //     };
-      
-    //         //     noteContainer.notes.push(note);
-    //         //   });
-    //         noteContainers.push(noteContainer);
-    //       });
-
-    //       // Sort noteContainers based on the index
-    //       noteContainers.sort((a, b) => a.index - b.index);
-
-    //       // Sort notes array inside each container based on the index
-    //       noteContainers.forEach((container) => {
-    //         container.notes.sort((a, b) => a.index - b.index);
-    //       });
-
-    //       // setFetching(false);
-    //       // setTimeout(() => {
-    //       setState(noteContainers);
-    //       // }, 200);
-    //       console.log(noteContainers, state);
-
-    //       // Clean up the snapshot listener for notes when the noteContainer is removed
-    //       // const containerUnsubscribe = onSnapshot(
-    //       //   noteContainerDoc.ref,
-    //       //   (containerSnapshot) => {
-    //       //     if (!containerSnapshot.exists()) {
-    //       //       // Remove the noteContainer from the noteContainers array
-    //       //       const containerIndex = noteContainers.findIndex(
-    //       //         (container) => container.id === noteContainer.id
-    //       //       );
-    //       //       if (containerIndex !== -1) {
-    //       //         noteContainers.splice(containerIndex, 1);
-    //       //         setState(noteContainers);
-    //       //       }
-    //       //       containerUnsubscribe(); // Stop listening to changes for this noteContainer
-    //       //       notesUnsubscribe(); // Stop listening to changes for the notes inside this noteContainer
-    //       //     }
-    //       //   }
-    //       // );
-    //     } catch (err) {
-    //       console.log(err);
-    //     }
-    //   });
-    // };
-    
     let unsubscribe
     console.log(user)
     if (user) {
@@ -202,11 +109,6 @@ function NotesDisplay() {
     // }
 
   }, [user]);
-
-  useEffect(() => {
-    console.log(state)
-  }, [state])
-  
   
 
   const updateIds = async (
@@ -449,228 +351,24 @@ function NotesDisplay() {
     toast.success("Note list created");
   };
 
-  const deleteCard = async (index: number) => {
-    if (user) {
-      try {
-        const userDocRef = doc(db, "users", user.uid);
-        const noteContainerRef = doc(
-          userDocRef,
-          "noteContainer",
-          state[index].id
-        );
-
-        await deleteDoc(noteContainerRef);
-      } catch (err) {
-        console.log(err);
-      }
-    }
-
-    // const newState = [...state];
-    // newState.splice(index, 1);
-    // setState(newState);
-
-    toast.success("Note list deleted");
-  };
-
-  const addElement = async (container: any, index: number) => {
-    console.log('adding')
-    let newNoteRef;
-    if (user) {
-      try {
-        const userDocRef = doc(db, "users", user?.uid);
-        const noteContainerRef = doc(userDocRef, "noteContainer", container.id);
-
-        const noteData = {
-          isEditable: false,
-          content: "Tap to Edit",
-          index: container.notes.length
-            ? container.notes[container.notes.length - 1].index + 1
-            : 0,
-        };
-
-        newNoteRef = await addDoc(
-          collection(noteContainerRef, "notes"),
-          noteData
-        );
-
-        await toast.promise(
-          new Promise<void>((resolve) => {
-            resolve();
-          }),
-          {
-            loading: "Loading",
-            success: "Note Created",
-            error: "Error while creating note",
-          }
-        );
-        // getUserNoteContainers(user.uid)
-      } catch (error) {
-        console.error("Error:", error);
-        toast.error("Error while creating note");
-      }
-    }
-
-    // console.log(!container.notes.length)
-  //   if (container.notes.length) {
-  //     console.log('running')
-  //     const newNotes = [
-  //       ...[
-  //         {
-  //           content: "Tap to Edit",
-  //         isEditable: false,
-  //         id: user ? newNoteRef?.id : Date.now().toString(),
-  //         index: container.notes.length
-  //         ? container.notes[container.notes.length - 1].index + 1
-  //         : 0,
-  //       },
-  //     ],
-  //   ];
-  //   container.notes = newNotes;
-  //   const tempState = [...state];
-  //   tempState.splice(index, 1, container);
-  //   setState(tempState);
-  // }
-  };
-
-  const handleNameChange = (
-    e: React.ChangeEvent<HTMLInputElement>,
-    objectId: string
-  ) => {
-    const updatedData = state.map((object) => {
-      if (object.id === objectId) {
-        return { ...object, name: e.target.value };
-      }
-      return object;
-    });
-    setState(updatedData);
-  };
-
-  const editName = async (containerId: string) => {
-    if (user) {
-      const noteContainerRef = doc(
-        db,
-        "users",
-        user.uid,
-        "noteContainer",
-        containerId
-      );
-
-      const container = state.find((obj) => obj.id === containerId);
-      if (container) {
-        const newName = { name: container.name };
-        await updateDoc(noteContainerRef, newName);
-      }
-    }
-  };
-
-  const toggleEditMode = async (containerId: string) => {
-    const updatedData = state.map((object) => {
-      if (object.id === containerId) {
-        return { ...object, isEditable: !object.isEditable };
-      }
-      return object;
-    });
-    setState(updatedData);
-
-    editName(containerId);
-  };
-  
-  const uploadImage = async (e, container: any, index: number) => {
-    const imageUpload = e.target.files[0];
-
-    const uniqueName =
-      Math.random().toString(36).substring(2, 12) + Date.now().toString();
-
-    const imageRef = ref(storage, uniqueName);
-    await uploadBytes(imageRef, imageUpload);
-
-    const pathRef = ref(storage, uniqueName);
-    const url = await getDownloadURL(pathRef);
-
-    let newNoteRef;
-    if (user) {
-      try {
-        const userDocRef = doc(db, "users", user?.uid);
-        const noteContainerRef = doc(userDocRef, "noteContainer", container.id);
-
-        const noteData = {
-          isEditable: false,
-          content: url,
-          index: container.notes.length
-            ? container.notes[container.notes.length - 1].index + 1
-            : 0,
-        };
-
-        newNoteRef = await addDoc(
-          collection(noteContainerRef, "notes"),
-          noteData
-        );
-
-        await toast.promise(
-          new Promise<void>((resolve) => {
-            resolve();
-          }),
-          {
-            loading: "Loading",
-            success: "Note Created",
-            error: "Error while creating note",
-          }
-        );
-      } catch (error) {
-        console.error("Error:", error);
-        toast.error("Error while creating note");
-      }
-    }
-
-    const newNotes = [
-      ...container.notes,
-      ...[
-        {
-          content: url,
-          isEditable: false,
-          id: user ? newNoteRef?.id : Date.now().toString(),
-          index: container.notes.length
-            ? container.notes[container.notes.length - 1].index + 1
-            : 0,
-        },
-      ],
-    ];
-    container.notes = newNotes;
-    const tempState = [...state];
-    tempState.splice(index, 1, container);
-    setState(tempState);
-  };
-
   return (
     <div className="w-full grow bg bg-no-repeat bg-cover bg-blue-700">
       <Gradient />
       {fetching ? <Loader /> : (
         <div className="flex flex-wrap gap-5 max-w-7xl mx-auto mt-10 px-2 md:px-10 items-start">
+
           <DragDropContext onDragEnd={onDragEnd}>
             {state.map((el, ind) => (
-              <NoteCard state={state} seState={setState} container={el} index={ind} />
+              <NoteCard state={state} seState={setState} container={el} index={ind} userId={user?.uid || ''} key={ind} />
             ))}
           </DragDropContext>
+
           <button className="p-2 w-60 px-3 py-3 min-w-[250px] rounded-2xl duration-200 delay-75 bg-[#F1F2F4] hover:bg-gray-300 h-16 ease-out opacity-40" type="button"
-            onClick={() => {
-              !user &&
-                setState([
-                  ...state,
-                  {
-                    id: Date.now().toString(),
-                    name: `Untitled ${state.length + 1}`,
-                    notes: [],
-                    isEditable: false,
-                    index: state.length,
-                  },
-                ]);
-              user && addNoteContainer();
-            }}
-          >
-            <span className="opacity-100 text-black font-regular">
-              + Add new list
-            </span>
+            onClick={() => {!user && setState([...state, { id: Date.now().toString(), name: `Untitled ${state.length + 1}`, notes: [], isEditable: false, index: state.length}]);
+            user && addNoteContainer()}}>
+            <span className="opacity-100 text-black font-regular">+ Add new list</span>
           </button>
+
         </div>
       )}
     </div>
