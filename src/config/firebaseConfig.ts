0b2c4cc04875c44a35c3ce7bf48deb69a5258bba
Hassan <muhammadhassanchannel786@gmail.com>
2023-06-26 16:38:39 +0500

    improved security

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -4,29 +4,19 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 import { getFirestore } from "firebase/firestore";
 import { getStorage }  from 'firebase/storage'
 
-// import dotenv from 'dotenv';
+import dotenv from 'dotenv';
 
-// dotenv.config();
-// console.log(import.meta.env.VITE_APIKEY)
-
-// const firebaseConfig = {
-//     apiKey: import.meta.env.VITE_APIKEY,
-//     authDomain: import.meta.env.VITE_AUTHDOMAIN,
-//     projectId: import.meta.env.VITE_PROJECTID,
-//     storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-//     messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-//     appId: import.meta.env.VITE_APPID,
-//     measurementId: import.meta.env.VITE_MEASUREMENTID
-// };
+dotenv.config();
+console.log(import.meta.env.VITE_APIKEY)
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyCoPZi47yVsJO5h7npnRApejG6Ecy2B0KU',
-    authDomain: 'trello-clone-typescript-fd86c.firebaseapp.com',
-    projectId: 'trello-clone-typescript-fd86c',
-    storageBucket: 'trello-clone-typescript-fd86c.appspot.com',
-    messagingSenderId: '89932851373',
-    appId: '1:89932851373:web:d957c092d0faf0cf513e24',
-    measurementId: 'G-X6P1KDF9BE'
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APPID,
+    measurementId: import.meta.env.VITE_MEASUREMENTID
 };
 
 
